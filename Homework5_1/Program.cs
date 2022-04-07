f19389ce8da3baac68aad364ae12fd5ba9487dd5
Welgr <ruchey44@gmail.com>
2022-04-07 18:54:17 +0500

    homework5
    homework5

---
+++ Homework5_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Homework5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите необходимый текст: ");
+            string filename = "text.txt";
+            File.WriteAllText(filename, Console.ReadLine());
+        }
+    }
+}
---
+++ Homework5_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+            File.AppendAllText(filename, "Время запуска: " + DateTime.Now + "\n");
+        }
+    }
+}
